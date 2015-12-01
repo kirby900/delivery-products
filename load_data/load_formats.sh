27778854ee31a909310844c482ebedafde8c98a2
kirby900 <kirby900@luckypucky.com>
2015-11-30 22:54:53 -0700

    Delete load_formats.sh

--- load_data/load_formats.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl "http://localhost:1337/productFormats/create?prdctFrmtNam=Transaction+file&entrpPrdctGid=1" && \
-curl "http://localhost:1337/productFormats/create?prdctFrmtNam=Pharmacy+file&entrpPrdctGid=1" && \
-curl "http://localhost:1337/productFormats/create?prdctFrmtNam=Control+file&entrpPrdctGid=1" && \
-curl "http://localhost:1337/productFormats/create?prdctFrmtNam=Prescription+file&entrpPrdctGid=2" && \
-curl "http://localhost:1337/productFormats/create?prdctFrmtNam=Pharmacy+file&entrpPrdctGid=2"
-
