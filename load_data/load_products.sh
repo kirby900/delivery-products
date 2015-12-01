db9fe0f2d8ca96b2f22145f1da94a8f4f8ccc2c4
kirby900 <kirby900@luckypucky.com>
2015-11-30 22:55:09 -0700

    Delete load_products.sh

--- load_data/load_products.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl "http://localhost:1337/enterpriseProducts/create?entrpPrdctNam=Prescriber+Rx" && \
-curl "http://localhost:1337/enterpriseProducts/create?entrpPrdctNam=Weekly+Launchtrac" && \
-curl "http://localhost:1337/enterpriseProducts/create?entrpPrdctNam=Customer+Source"
-
