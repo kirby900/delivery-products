beaa278e1251374dddb46979b8bb40ff9310974a
Kirby Wirby <kirby900@luckypucky.com>
2015-12-28 22:53:31 -0700

    updated main nav links to load from ng-repeat

--- assets/js/app_controller.js
+++ assets/js/app_controller.js
@@ -2,6 +2,12 @@ angular.module('app')
 .controller('appController', ['$scope', function($scope){
   console.log('Entered appController');
 
+  $scope.links = [
+    { label: 'Home', url: '#/home' },
+    { label: 'Products', url: '#/products/list' },
+    { label: 'Parameters', url: '#/parameters/list' }
+  ];
+
   $scope.indicatorToText = function(ind){
     return ind == "Y" ? 'Yes' : 'No';
   };
